2d4bb88f8970f1df5b57f346ff393020814591c5
Justin Zwart <justinzwart314@gmail.com>
2021-07-12 13:33:06 -0400

    Adapted main to be more user friendly

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
 from minmax import minmax
 import chess
 
-
+white = True
 board = chess.Board()
 
 # game loop
-while True:
-    board.push(minmax(4, True, board, -100000, 100000))
-    board.push(input())
+if white:
+    while True:
+        move = minmax(4, True, board, -100000, 100000)
+        board.push(move)
+        print(move)
+        board.push(input())
+else:
+    while True:
+        board.push(input())
+        move = minmax(4, True, board, -100000, 100000)
+        board.push(move)
+        print(move)
