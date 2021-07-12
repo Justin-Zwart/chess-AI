e6b570ac702fe548fe93b7f862a2e87172639ba2
Justin Zwart <justinzwart314@gmail.com>
2021-07-12 13:29:48 -0400

    Created crude static evaluation
    Static evaluation will be later replaced with a better function. This is used to test main.py and minimax. Uses only chess points system.

--- static_evaluation.py
+++ static_evaluation.py
@@ -1,2 +1,32 @@
-def static_evaluation():
-    return
+def static_evaluation(board):
+    board = board.fen()
+    evaluation = 0
+
+    for i in range(len(board)):
+        # white pieces 
+        if board[i] == 'q':
+            evaluation += 9
+        if board[i] == 'r':
+            evaluation += 5
+        if board[i] == 'n':
+            evaluation += 3
+        if board[i] == 'b':
+            evaluation += 3
+        if board[i] == 'p':
+            evaluation += 1
+
+        # black pieces
+        if board[i] == 'Q':
+            evaluation -= 9
+        if board[i] == 'R':
+            evaluation -= 5
+        if board[i] == 'N':
+            evaluation -= 3
+        if board[i] == 'B':
+            evaluation -= 3
+        if board[i] == 'P':
+            evaluation -= 1
+
+
+    return evaluation
+
