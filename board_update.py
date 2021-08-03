63ee4b3fad9535064a552a4f85ca7ad490962894
Justin Zwart <justinzwart314@gmail.com>
2021-08-03 08:13:05 -0400

    Switched letters in static evaluation

--- board_update.py
+++ board_update.py
@@ -1,2 +1,3 @@
 def board_update(board_san, board_fen, move_san):
+    
     return board_san, board_fen
--- static_evaluation.py
+++ static_evaluation.py
@@ -1,30 +1,29 @@
-def static_evaluation(board):
-    board = board.fen()
+def static_evaluation(board_fen):
     evaluation = 0
 
     for i in range(len(board)):
         # white pieces 
-        if board[i] == 'Q':
+        if board_fen[i] == 'Q':
             evaluation += 9
-        if board[i] == 'R':
+        if board_fen[i] == 'R':
             evaluation += 5
-        if board[i] == 'N':
+        if board_fen[i] == 'N':
             evaluation += 3
-        if board[i] == 'B':
+        if board_fen[i] == 'B':
             evaluation += 3
-        if board[i] == 'P':
+        if board_fen[i] == 'P':
             evaluation += 1
 
         # black pieces
-        if board[i] == 'q':
+        if board_fen[i] == 'q':
             evaluation -= 9
-        if board[i] == 'r':
+        if board_fen[i] == 'r':
             evaluation -= 5
-        if board[i] == 'n':
+        if board_fen[i] == 'n':
             evaluation -= 3
-        if board[i] == 'b':
+        if board_fen[i] == 'b':
             evaluation -= 3
-        if board[i] == 'p':
+        if board_fen[i] == 'p':
             evaluation -= 1
 
 
